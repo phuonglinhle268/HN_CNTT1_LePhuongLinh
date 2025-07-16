eac742dd0b0ca0fa595672be0ba23b4f2d27c4cc
phuonglinhle268 <plinhle26@gmail.com>
2025-07-16 09:54:21 +0700

    Add files via upload

---
+++ Bai2.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Question {
+    int id;
+    char content[100];
+} Question;
+
+typedef struct Stack {
+    Question* questions;
+    int top;
+    int capacity;
+} Stack;
+
+typedef struct Queue {
+    Question* questions;
+    int front;
+    int rear;
+    int capacity;
+} Queue;
+
+Stack* createStack(int capacity) {
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    stack -> capacity = capacity;
+    stack -> top = -1;
+    return stack;
+}
+Queue createQueue(int capacity) {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue -> capacity = capacity;
+    queue -> front = 0;
+    queue -> rear = -1;
+    return queue;
+}
+void push(Stack* stack, Question question) {
+    if (stack -> top == stack -> capacity -1) {
+        printf("Stack da day\n");
+        return;
+    }
+    stack -> top++;
+    stack -> questions[stack -> top] = question;
+}
+void enqueue(Queue* queue, Question question) {
+    if (queue -> rear == queue -> capacity - 1) {
+        printf("Queue da day\n");
+        return;
+    }
+    queue -> rear++;
+    queue -> questions[queue -> rear] = question;
+}
+int main() {
+    int choice;
+    Stack* practiceStack = createStack();
+    Stack* redoStack = createStack();
+    Queue fullHistory = createQueue();
+    do {
+        printf("\n-----INTERVIEW MANAGER-----\n");
+        printf("1. Practice\n");
+        printf("2. Undo\n");
+        printf("3. Redo\n");
+        printf("4. History\n");
+        printf("5. Exit\n");
+        printf("Nhap lua chon cua ban: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                Question question;
+                printf("Nhap ID cho cau hoi: ");
+                scanf("%d", question.id);
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                printf("EXIT\n");
+                break;
+            default:
+                printf("Lua chon khong hop le\n");
+        }
+    } while (choice != 5);
+    return 0;
+}
+
